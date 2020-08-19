e85fb14ca3b9e14d215006cf568f29b1ff88b986
bricktsre <gwoodward21@gmail.com>
2020-08-19 15:48:28 -0400

    first_commit

---
+++ tweetRetriever.py
@@ -0,0 +1,62 @@
+import os
+import re
+import math
+import time
+
+import GetOldTweets3 as got
+import numpy as np
+from datetime import datetime
+class TweetRetriever:
+	tweetLength = None
+
+	def __init__(self, outfile):
+		self.outfile = outfile
+		if os.path.exists(self.outfile):
+			os.remove(self.outfile)
+
+	def getTweets(self,handle):
+		dataset = []
+		total = 0
+		for year in range(2006,2021):
+			print("Starting to pull tweets from ", year)
+			sinceDate = str(year) + "-01-01"
+			untilDate = str(year+1) + "-01-01"
+			since = datetime.strptime(sinceDate, '%Y-%m-%d')
+			until = datetime.strptime(untilDate, '%Y-%m-%d')
+			tweetCriteria = got.manager.TweetCriteria().setUsername(handle)\
+							   .setEmoji("unicode")\
+							   .setSince(sinceDate)\
+							   .setUntil(untilDate)
+			self.tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+			lengths, count = self.cleanAndCount()
+			dataset.extend(lengths)
+			total += count
+			print("Pulled down ", count," tweets from ", year, ", sleeping for 15 minutes before trying next year")
+			if count > 0:
+				time.sleep(900)
+		return self.calculateMeanAndStdDev(dataset, total)
+
+	def removeLinks(self,text):
+		return re.sub(r'''(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))'''," ", text)
+
+	def calculateMeanAndStdDev(self, dataset, count):
+		mean = sum(dataset) / count
+		total = 0
+		for item in dataset:
+			total += (item - mean) ** 2
+		std = math.sqrt(total / count)
+		return mean, std
+
+	def cleanAndCount(self):
+		tweetLengths = []
+		count = 0
+		file = open(self.outfile, "a+")
+		for tweet in self.tweets:
+			tweet = self.removeLinks(tweet.text) + ' '
+			if tweet == " ":
+				continue
+			tweetLengths.append(len(tweet))
+			count += 1
+			file.write(tweet)
+		file.close()
+		return tweetLengths, count
